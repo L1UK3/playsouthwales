7de8b326a0e65530c757a085c49eb718c9f009bf
Luke <lukeenness@outlook.com>
2026-06-07 21:42:08 +0100

    Added clerk dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import '@styles/global.css'
 import '@styles/animations.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ClerkProvider } from '@clerk/react'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -30,24 +30,10 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</ClerkProvider>
 	</StrictMode>
-=======
-import './index.css'
-import '@playwales/shared/styles/global.css'
-import '@playwales/shared/styles/animations.css'
-import App from './App.tsx'
-import { ClerkProvider } from '@clerk/react'
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    //takes in api key for clerk
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-	</StrictMode>,
->>>>>>> 19ad77c63b5164f0321453f7161b73fea7b0cf81
 )
