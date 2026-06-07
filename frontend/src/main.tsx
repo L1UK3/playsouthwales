a64976511952dca2c4140a6fa9cf8c5ab13595c0
Luke <lukeenness@outlook.com>
2026-06-07 20:34:44 +0100

    Forgot to define queryClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import '@styles/global.css'
 import '@styles/animations.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -15,8 +16,20 @@ declare module '@tanstack/react-router' {
 	}
 }
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 5,
+			retry: 1
+		}
+	}
+})
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</StrictMode>
 )
