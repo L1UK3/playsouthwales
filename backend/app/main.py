83702b42c1c12a86ad1bd308722a0f18aa0a4b7a
Luke Enness <lukeenness@outlook.com>
2026-06-27 11:19:21 +0100

    added access control allow origin header

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=[
+        "Access-Control-Allow-Origin",
         "Content-Type",
         "Authorization",
         "Accept",
