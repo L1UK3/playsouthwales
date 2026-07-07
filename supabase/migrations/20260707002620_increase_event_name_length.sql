bd98524aaec3eb644a46e8e259eddc1739dca88a
Luke <lukeenness@outlook.com>
2026-07-07 16:03:43 +0100

    increase event name length so that fetched data can be added to the database

---
+++ supabase/migrations/20260707002620_increase_event_name_length.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.events ALTER COLUMN name TYPE text;
+ALTER TABLE public.events ALTER COLUMN "ticketLink" TYPE text;
+
+ALTER TABLE public.weekly_events ALTER COLUMN name TYPE text;
+ALTER TABLE public.weekly_events ALTER COLUMN "ticketLink" TYPE text;
