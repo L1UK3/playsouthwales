9f401b53464d0aeda50d8e1d087917786e81001d
L1UK3 <lukeenness@outlook.com>
2026-09-25 14:43:57 +0100

    removed bot - it will be hosted on another repo

--- bot/src/types/discord.d.ts
+++
@@ -1,9 +0,0 @@
-import "discord.js";
-
-declare module "discord.js" {
-    interface ClientEvents {
-        dailyUpdate: [message: string, channelId?: string];
-        weeklyUpdate: [message: string, channelId?: string];
-        monthlyUpdate: [message: string];
-    }
-}
