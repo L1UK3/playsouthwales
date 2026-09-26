48e374d7a68d262e8c61261248eb5696d090f7c9
L1UK3 <lukeenness@outlook.com>
2026-09-26 15:29:01 +0100

    updated DiscordService

--- backend/app/integrations/discord_bot.py
+++ backend/app/integrations/discord_bot.py
@@ -1,30 +1,57 @@
+import datetime
 import logging
 
 import discord
 
 from app.config import get_settings
+from app.dependencies import supabase
+
+from ..services.event import get_events_from_db
 
 settings = get_settings()
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
 
 logger = logging.getLogger(__name__)
 
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+class DiscordService(discord.Client):
+    user: discord.ClientUser
+    channel_id: str = settings.discord_channel_id
+
+    async def on_ready(self):
+        logger.info(f"Discord bot ready: {self.client.user}")
+
+    async def send_daily_update(self):
+        channel = self.get_channel(self.channel_id)
+        if not channel:
+            logger.error(f"Channel with ID {self.channel_id} not found.")
+
+        events = await get_events_from_db(
+            db=supabase,
+            day=datetime.datetime.now(datetime.UTC).date(),
+        )
+
+        message_content = "Daily Events Breakdown:\n\n"
+        for event in events:
+            message_content += f"- {event['name']} on {event['date']}\n"
+
+        await channel.send(message_content)
+
+    async def send_weekly_update(self):
+        channel = self.get_channel(self.channel_id)
+        if not channel:
+            logger.error(f"Channel with ID {self.channel_id} not found.")
+
+        events = await get_events_from_db(db=supabase, weekly=True)
+
+        message_content = "Upcoming Weekly Events:\n\n"
+        for event in events:
+            message_content += f"- {event['name']} on {event['date']}\n"
+
+        await channel.send(message_content)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+intents = discord.Intents.default()
+intents.message_content = True
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-
-
+client = DiscordService(intents=intents)
 client.run(settings.discord_bot_token, log_handler=None)
